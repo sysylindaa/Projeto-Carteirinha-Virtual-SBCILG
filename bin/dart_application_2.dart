ad6ee4c49cb7abda4b5e4fb281167f70805f17a3
Your Name <you@example.com>
2022-10-03 10:52:05 -0300

    resolve conflitos

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,5 +1,54 @@
-import 'package:dart_application_2/dart_application_2.dart' as dart_application_2;
-
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_2.calculate()}!');
+}
+class Usuario{
+  String nomeUsuario; 
+  String nome;
+  String matricula;
+  String email: 
+
+
+  Usuario(this.nomeUsuario, this.nome, this.matricula, this.email); 
+  void cadastrar(){
+
+  }
+
+
+  }
+
+
+
+abstract class Turma{
+  String nome;
+
+  Turma(this.nome);
+}
+
+class Aluno {
+  String nome;
+  String matricula;
+  int data_nasc;
+  int anoAtual;
+  String campus;
+  String cpf;
+  String rg;
+  Turma turma;
+
+  Aluno(this.nome, this. matricula, this.data_nasc, this.anoAtual, this.campus, this.cpf, this.rg, this.turma);
+
+  int idade(){
+    int idade = anoAtual - data_nasc;
+    return idade;
+  }
+
+}
+
+class Professor{
+  String nome;
+  String matricula;
+  Turma turma;
+
+  Professor(this.nome, this.matricula, this.turma);
+}
+class Diretor{
+
 }
