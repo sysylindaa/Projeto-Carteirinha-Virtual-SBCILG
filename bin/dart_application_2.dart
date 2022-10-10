1f2bef7db963a280d74999040d6433ddfef2ef49
rebequinha01 <112480230+rebequinha01@users.noreply.github.com>
2022-10-10 11:18:42 -0300

    Update dart_application_2.dart

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -4,18 +4,14 @@ class Usuario{
   String nomeUsuario; 
   String nome;
   String matricula;
-  String email: 
+  String email;
 
 
   Usuario(this.nomeUsuario, this.nome, this.matricula, this.email); 
   void cadastrar(){
 
   }
-
-
-  }
-
-
+}
 
 abstract class Turma{
   String nome;
@@ -42,13 +38,50 @@ class Aluno {
 
 }
 
-class Professor{
+class PCampus {
+  String data;
+  String hora;
+  double presenca;
+
+  PCampus(this.data, this.hora, this.presenca);
+}
+
+class Professor {
   String nome;
   String matricula;
   Turma turma;
 
   Professor(this.nome, this.matricula, this.turma);
 }
-class Diretor{
 
+class Disciplina {
+  String nome;
+  Professor professor;
+  Turma turma;
+
+  Disciplina(this.nome, this.professor, this.turma);
+}
+
+class Coordenador { 
+  Usuario n_usuario;
+  Usuario matricula;
+  Usuario email;
+
+  Coordenador(this.n_usuario, this.matricula, this.email);
+
+}
+
+class Aula {
+  String data;
+  String horario;
+  Disciplina disciplina;
+
+  Aula(this.data, this.horario, this.disciplina);
+
+}
+
+class PAula {
+  double presente;
+
+  PAula(this.presente);
 }
